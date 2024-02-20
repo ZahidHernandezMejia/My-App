877fef14b65c03699b4f27cf6649f9ff67cff4b0
Zahid DP <zahidhm.dp@gmail.com>
2024-02-20 12:19:39 -0600

    Stylesheet

--- AwesomeTSProject/src/screens/ContadorScreen.tsx
+++ AwesomeTSProject/src/screens/ContadorScreen.tsx
@@ -1,24 +1,11 @@
 import React, {useState} from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
 
 export const ContadorScreen = () => {
   const [contador, setContador] = useState(1);
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-      }}>
-      <Text
-        style={{
-          color: 'black',
-          textAlign: 'center',
-          fontSize: 50,
-          // Relative es la propiedad que tiene por defecto
-          position: 'relative',
-          top: -15,
-        }}>
+    <View style={styles.container}>
+      <Text style={styles.title}>
         Contador: {contador}
       </Text>
 
@@ -28,13 +15,29 @@ export const ContadorScreen = () => {
         color="#52BFC8"
         accessibilityLabel="Learn more about this purple button"
       /> */}
-        <TouchableOpacity
-        onPress={()=>setContador(contador+1)}
-        >
-            <View style={{backgroundColor:'#52BFC8'}}>
-            <Text style={{color:'black', fontSize:25}}>+1</Text>
-            </View>
-        </TouchableOpacity>
+      <TouchableOpacity onPress={() => setContador(contador + 1)}>
+        <View style={styles.buttonIncrement}>
+          <Text style={{color: 'black', fontSize: 25}}>+1</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center'
+    },
+    title:{
+        color: 'black',
+        textAlign: 'center',
+        fontSize: 50,
+        // Relative es la propiedad que tiene por defecto
+        position: 'relative',
+        top: -15,
+    },
+    buttonIncrement:{
+        backgroundColor: '#52BFC8',
+    }
+});
