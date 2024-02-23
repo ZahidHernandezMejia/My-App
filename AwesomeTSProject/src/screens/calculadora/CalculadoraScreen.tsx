127f7f35ef767b8fa292da25c5151023c071252d
Zahid DP <zahidhm.dp@gmail.com>
2024-02-23 12:05:56 -0600

    DESIGN AND BUTTON BASE

--- AwesomeTSProject/src/screens/calculadora/CalculadoraScreen.tsx
+++ AwesomeTSProject/src/screens/calculadora/CalculadoraScreen.tsx
@@ -1,17 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StatusBar, Text, View } from 'react-native'
 import { styles } from '../../theme/appTheme'
+import { BotonCalc } from '../../components/BotonCalc';
 
+const a =()=>{
+    console.log("sdaf");
+} ;
 export const CalculadoraScreen = () => {
-  return (
-    <View>
-        <StatusBar
-        backgroundColor={'black'}
-        barStyle={'light-content'}
-        />
-        <Text style={styles.texto}>
-        CalculadoraScreen
-        </Text>
-    </View>
-  )
+    const [numero, setNumero] = useState('100');
+
+    const [numeroAnterior, setNumeroAnterior] = useState('0');
+    
+    const limpiar =()=>{
+        setNumero('0');
+    }
+
+    const armarNumero=(numeroTexto:string)=>{
+        setNumero(numero+numeroTexto)
+    }
+    return (
+        <View style={styles.calculadoraContainer}>
+            <StatusBar
+                backgroundColor={'black'}
+                barStyle={'light-content'}
+            />
+            <Text style={styles.resultadoPeque}>{numeroAnterior}</Text>
+            <Text style={styles.resultado}
+                numberOfLines={1}
+                adjustsFontSizeToFit
+
+            >{numero} </Text>
+            <View style={styles.fila}>
+                <BotonCalc text="C" color="#ADB8B8" action={limpiar} />
+                <BotonCalc text="+/-" color="#ADB8B8" action={limpiar}/>
+                <BotonCalc text="del" color="#ADB8B8" action={limpiar}/>
+                <BotonCalc text="/" color="#FF9427" action={limpiar}/>
+            </View>
+            <View style={styles.fila}>
+                <BotonCalc text="7" action={armarNumero} />
+                <BotonCalc text="8" action={armarNumero} />
+                <BotonCalc text="9" action={armarNumero} />
+                <BotonCalc text="X" color="#FF9427" action={limpiar}/>
+            </View>
+            <View style={styles.fila}>
+                <BotonCalc text="4" action={armarNumero} />
+                <BotonCalc text="5" action={armarNumero} />  
+                <BotonCalc text="6" action={armarNumero} />  
+                <BotonCalc text="-" color="#FF9427" action={limpiar}/>
+            </View>
+            <View style={styles.fila}>
+                <BotonCalc text="1" action={armarNumero} />
+                <BotonCalc text="2" action={armarNumero} />
+                <BotonCalc text="3" action={armarNumero} />
+                <BotonCalc text="+" color="#FF9427" action={limpiar}/>
+            </View>
+            <View style={styles.fila}>
+                <BotonCalc text="0" ancho action={armarNumero} />
+                <BotonCalc text="." action={limpiar}/>
+                <BotonCalc text="=" color="#FF9427" action={limpiar}/>
+            </View>
+        </View>
+    )
 }
--- AwesomeTSProject/src/theme/appTheme.tsx
+++ AwesomeTSProject/src/theme/appTheme.tsx
@@ -1,12 +1,46 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    fondo:{
-        flex:1,
-        backgroundColor:'black',
+    fondo: {
+        flex: 1,
+        backgroundColor: 'black',
     },
-    texto:{
-        color:'white',
-        fontSize:25
+    calculadoraContainer: {
+        paddingHorizontal: 20,
+        backgroundColor: 'blue',
+        flex: 1,
+        justifyContent: "flex-end",
+
+    },
+    resultado: {
+        color: 'white',
+        fontSize: 60,
+        textAlign: 'right',
+        marginBottom:10
+    },
+    resultadoPeque: {
+        color: 'rgba(255,255,255,0.5)',
+        fontSize: 30,
+        textAlign: 'right'
+    },
+    fila:{
+        flexDirection:"row",
+        justifyContent:'center',
+        marginBottom:18,
+        paddingHorizontal:10
+    },
+    boton: {
+        height: 80,
+        width: 80,
+        borderRadius: 100,
+        justifyContent: 'center',
+        backgroundColor: '#ADB8B8',
+        marginHorizontal:10
+    },
+    botonTexto: {
+        textAlign:'center',
+        fontSize:30,
+        color:'black',
+        fontWeight:'300',
     }
 });
